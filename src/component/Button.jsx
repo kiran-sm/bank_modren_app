79d312eec358309bc937687d46b55fafb509d3df
Kiran <smkiran.09@gmail.com>
2024-05-03 18:35:53 +0530

    Billing Section Completed

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button "
+      className={`py-4 px-6 bg-blue-gradient font-poppins text-primary font-medium text-[18px] outline-none ${styles} `}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
